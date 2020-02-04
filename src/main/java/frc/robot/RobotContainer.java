48f3c2691b95cdde97d7f3567437541007dce670
Leo Shierts <leoshierts@gmail.com>
2020-02-04 06:59:21 -0600

    Nothing Really lol

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,13 +9,21 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
+// import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import static edu.wpi.first.wpilibj.Joystick.ButtonType;
+
+import frc.robot.Constants.*;
+
+import java.util.Set;
 
 // import edu.wpi.first.wpilibj.Joystick;
 // import edu.wpi.first.wpilibj.buttons.Button;
@@ -28,23 +36,36 @@ import static edu.wpi.first.wpilibj.Joystick.ButtonType;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  private Joystick driveJoystick = new Joystick(0);
+  private Joystick beefCakeJoystick = new Joystick(1);
+
+  // The robot's subsystems are defined here...
   private final Drive drive = new Drive();
-  private final DriveForwardXFeet driveXFeet = new DriveForwardXFeet(drive, 10.0);
   private final BeefCake beefCake = new BeefCake();
+
+  // The robot's commands are defined here...
+  private BeefCakeJoystickAngle beefCakeJoystickAngle = new BeefCakeJoystickAngle(beefCake, beefCakeJoystick);
+  private final DriveForwardXFeet driveXFeet = new DriveForwardXFeet(drive, 10.0, 0.5);
+  private final DriveJoystick driveJoystickCommand = new DriveJoystick(drive, driveJoystick);
+
   private StartBeefCakeFeed startBeefCakeFeed = new StartBeefCakeFeed(beefCake);
 
-  Joystick beefCakeJoystick = new Joystick(1);
- 
-
-  /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
-   */
+  // The container for the robot.  Contains subsystems, OI devices, and commands.
   public RobotContainer() {
-    
 
-    // Configure the button bindings
     configureButtonBindings();
+
+    this.drive.setDefaultCommand(
+      new RunCommand(() -> drive.arcade(driveJoystick.getY()*-1.0, driveJoystick.getX()), 
+                           drive
+                    )
+    );
+
+    this.beefCake.setDefaultCommand(
+      new RunCommand(() -> beefCake.angleJoystick(beefCakeJoystick.getY()),
+                           beefCake
+                    )
+    );
   }
 
   /**
@@ -56,16 +77,84 @@ public class RobotContainer {
   private void configureButtonBindings() {  
     System.out.println("Setting Bindings");
 
-    new JoystickButton(beefCakeJoystick, 1)
-        .whenPressed(this.startBeefCakeFeed);  
-  
-    new JoystickButton(beefCakeJoystick, 2)
-      .whenPressed(this.startBeefCakeFeed);  
-    
-    new JoystickButton(beefCakeJoystick, 3)
-    .whenPressed(this.startBeefCakeFeed);  
-  }
+    //  Activate/Deactivate the feeder
+    new JoystickButton(this.beefCakeJoystick, 1)
+       .whenPressed(new InstantCommand(this.beefCake::feederOn, this.beefCake))
+       .whenReleased( this.beefCake::feederOff);
 
+    //  Change robot speed limit. Based on buttons 2 and 3
+    new JoystickButton(this.driveJoystick, 3)
+      .whenPressed(this.drive::setLimitFast)
+      .whenReleased(this.drive::setLimitNorm);
+    
+    new JoystickButton(this.driveJoystick, 2)
+      .whenPressed(this.drive::setLimitSlow)
+      .whenReleased(this.drive::setLimitNorm);
+
+    //  Activate/Deactivate the launcher wheels
+    new JoystickButton(this.beefCakeJoystick, 6)
+      .whenPressed(this.beefCake::launcherOn);
+
+    new JoystickButton(this.beefCakeJoystick, 7)
+      .whenPressed(this.beefCake::launcherOff);   
+
+    //  Runs TestSequentialCommandGroup command once
+      new JoystickButton(this.driveJoystick, 6)
+      .whenPressed(
+        new SequentialCommandGroup(
+          new DriveForwardXFeet(drive, 6.5, 0.8), 
+          new TurnXDegrees(drive, 62),
+          new DriveForwardXFeet(drive, 6.5, 0.8), 
+          new TurnXDegrees(drive, 62),
+          new DriveForwardXFeet(drive, 6.5, 0.8), 
+          new TurnXDegrees(drive, 62),
+          new DriveForwardXFeet(drive, 6.5, 0.8), 
+          new TurnXDegrees(drive, 62)
+        )
+      )
+      ;
+
+    //  Runs the DriveForwardXFeet command once
+    new JoystickButton(this.driveJoystick, 7)
+      .whenPressed(new DriveForwardXFeet(this.drive, 6.5, 0.8))
+      ;
+  
+    //  Turns the robot left or right 45 degrees ( +degrees is left, -degrees is right)
+    new JoystickButton(this.driveJoystick, 8)
+    .whenPressed(new TurnXDegrees(this.drive, 45.0))
+    ;
+   
+    new JoystickButton(this.driveJoystick, 9)
+    .whenPressed(new TurnXDegrees(this.drive, -45.0))
+    ;
+   
+
+    //  Reverses the drive direction for Shooting vs Intake
+    new JoystickButton(this.driveJoystick, 11)
+      .whenPressed(new InstantCommand(this.drive::drivingMode, this.drive))
+    ;
+
+    new JoystickButton(this.driveJoystick, 10)
+      .whenPressed(new InstantCommand(this.drive::aimingMode, this.drive))
+    ;
+
+
+    //  Angles the launcher with buttons
+    // new JoystickButton(this.beefCakeJoystick, 10)
+    //   .whenPressed(this.beefCake::adjustAngleUp)
+    //   .whenReleased(this.beefCake::stopAngle);
+    
+    // new JoystickButton(this.beefCakeJoystick, 11)
+    //   .whenPressed(this.beefCake::adjustAngleDown)
+    //   .whenReleased(this.beefCake::stopAngle);
+
+    // Control the intake
+    new JoystickButton(this.driveJoystick, 1)
+      .whenPressed(this.beefCake::intakeOn)
+      .whenReleased(this.beefCake::intakeOff);
+
+    }      
+    
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
@@ -74,6 +163,6 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     System.out.println("getting Autonomous Command");
-    return driveXFeet;
+    return new DriveForwardXFeet(this.drive, 2.0, 0.8);
   }
 }
